6761a34832a267f9d0e9bd3a2f86f4dcf457a1e5
Ryan Leyland <71075940+rsleyland@users.noreply.github.com>
2022-01-26 10:58:28 -0500

    Update Admin.model.js
    Changed export name

--- backend/models/Admin.model.js
+++ backend/models/Admin.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('./User.model.js');
 
-const ImmigrationOfficerSchema = new mongoose.Schema({
+const AdminSchema = new mongoose.Schema({
     nickname: {
         type: String,
         required: false
@@ -9,4 +9,4 @@ const ImmigrationOfficerSchema = new mongoose.Schema({
 });
 
 
-module.exports = User.discriminator('ImmigrationOfficer', ImmigrationOfficerSchema);
+module.exports = User.discriminator('Admin', AdminSchema);

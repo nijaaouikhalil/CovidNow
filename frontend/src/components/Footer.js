d7fe83d3ac252245702503965dc0ed4042dbe6cd
hackerrank <59776984+georgekouts108@users.noreply.github.com>
2022-02-20 16:55:28 -0500

    edited the footer frontend with an auto-changeable year

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,36 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
+// function Footer() {
+//   return (
+//     <footer>
+//       <Container>
+//         <Row>
+//           <Col className="text-center py-3">Copyright &copy; CovidTracker</Col>
+//         </Row>
+//       </Container>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
+
 function Footer() {
   return (
     <footer>
-      <Container>
-        <Row>
-          <Col className="text-center py-3">Copyright &copy; CovidTracker</Col>
-        </Row>
-      </Container>
+      <Navbar bg="primary" collapseOnSelect variant="dark">
+        <Container>
+          <Row>
+            <Col className="text-center py-3">&copy; {getCurrentYear()} CovIDTracker</Col>
+          </Row>
+        </Container>
+      </Navbar>
     </footer>
   );
+}
+
+function getCurrentYear() {
+  return (new Date()).getFullYear();
 }
 
 export default Footer;
